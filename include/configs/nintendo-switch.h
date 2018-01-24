5f600b8061511ef4b1824e0a307f5b0ac97fcb5e
Andre Heider <a.heider@gmail.com>
2018-01-24 18:48:56 +0100

    arm64: tegra: add support for Nintendo Switch
    fix sd card power gpio
    
    use a memlayout so shit doesnt scribble over one another
    
    reenable autoboot
    
    add second i2c bus
    
    get rid of sd wp pin settings
    
    no such thing
    
    set CONFIG_SEC_FIRMWARE_ARMV8_PSCI
    this also passes the psci relevant pieces to the kernel dtb
    
    clean up defconfig
    
    disable spi support
    no such thing on the switch
    
    lets not scribble over the stack
    
    Start separating out the fail in pinmux/gpio config
    
    More gpios
    
    Just comment out all the fail io config for now
    Better be safe than sorry
    
    Do not turn on backlight, do turn off LCD
    
    set CONFIG_REMAKE_ELF
    this created u-boot.elf out of the devicetree appended u-boot.bin.
    u-boot.elf can we used as a coreboot payload.
    
    defconfig: enable sdp
    
    defconfig: use "sdp 0" as boot command
    
    reorder $stuff in mem, again
    
    add i2c pins and rename i2c busses
    
    Turn on the wifi
    
    Co-authored-by: Andre Heider <a.heider@gmail.com>
    Co-authored-by: SwtcR <swtcr0@gmail.com>

---
+++ include/configs/nintendo-switch.h
@@ -0,0 +1,43 @@
+/*
+ * (C) Copyright 2013-2015
+ * NVIDIA Corporation <www.nvidia.com>
+ *
+ * SPDX-License-Identifier:     GPL-2.0+
+ */
+
+#ifndef _NINTENDO_SWITCH_H
+#define _NINTENDO_SWITCH_H
+
+#include <linux/sizes.h>
+
+#include "tegra210-common.h"
+
+#define CONFIG_REMAKE_ELF
+
+/* High-level configuration options */
+#define CONFIG_TEGRA_BOARD_STRING	"Nintendo Switch"
+
+/* Board-specific serial config */
+#define CONFIG_TEGRA_ENABLE_UARTB
+
+/* I2C */
+#define CONFIG_SYS_I2C_TEGRA
+
+/* USB2.0 Host support */
+#define CONFIG_USB_EHCI_TEGRA
+
+/* USB networking support */
+
+/* PCI host support */
+
+/* General networking support */
+
+#include "tegra-common-usb-gadget.h"
+#include "tegra-common-post.h"
+
+/* Crystal is 38.4MHz. clk_m runs at half that rate */
+#define COUNTER_FREQUENCY	19200000
+
+#define CONFIG_SYS_MEM_RESERVE_SECURE (4 * SZ_1M)
+
+#endif /* _NINTENDO_SWITCH_H */
